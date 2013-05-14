0b093ccd2824fc60e3dd7440e3889739e160416d
Pavel Talanov <talanov.pavel@gmail.com>
2013-05-14 14:22:50 +0400

    Total and prices view

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,30 @@
 class Order < ActiveRecord::Base
   attr_accessible :items, :prices, :table_num
+
+  def get_items
+    if items.nil?
+      return []
+    end
+    items.split(',').map do |s|
+      s.strip
+    end
+  end
+
+
+  def get_prices
+    if prices.nil?
+      return []
+    end
+    prices.split(',').map do |s|
+      s.strip.to_f
+    end
+  end
+
+  def total
+    sum = 0
+    get_prices.each do |p|
+      sum += p
+    end
+    '%.2f' % sum
+  end
 end
